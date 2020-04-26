65cbf479adf5aa28b1e80632902d2e1ba24bcb8e
Aiden Oliver <31047390+AidenOliver@users.noreply.github.com>
2020-04-25 18:29:46 -0600

    removed browser router links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import BookContextProvider from './Contexts/BookContext';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Dashboard from './Components/Dashboard';
 
